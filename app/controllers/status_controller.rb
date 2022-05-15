c2ff88338d433d911150c41c381b61cd3b9b4f3e
Oscar <orivas155@gmail.com>
2022-05-14 22:27:00 -0500

    fix parent controller of status controller

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Get current application status
-class StatusController < Sinatra::Base
+class StatusController < ApplicationController
   get "/status" do
     json(
       {
