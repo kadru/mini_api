567c83a531b93d4a886ffd60ed7fc4dc193a0e5e
Oscar <orivas155@gmail.com>
2022-05-14 21:48:52 -0500

    fix content type of status endpoint

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -3,9 +3,11 @@
 # Get current application status
 class StatusController < Sinatra::Base
   get "/status" do
-    {
-      enviroment: ENV.fetch("APP_ENV"),
-      db_connection: DB.run("select 1 + 1").nil? && :ok
-    }.to_json
+    json(
+      {
+        enviroment: ENV.fetch("APP_ENV"),
+        db_connection: DB.run("select 1 + 1").nil? && :ok
+      }
+    )
   end
 end
